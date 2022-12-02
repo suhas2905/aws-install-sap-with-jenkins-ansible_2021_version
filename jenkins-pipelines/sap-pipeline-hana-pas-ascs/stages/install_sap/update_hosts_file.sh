4af2ece47015da267de0f9f44f2b66bd89204211
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2022-12-02 07:59:42 -0500

    changed from public to private

--- jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/install_sap/update_hosts_file.sh
+++ jenkins-pipelines/sap-pipeline-hana-pas-ascs/stages/install_sap/update_hosts_file.sh
@@ -21,26 +21,26 @@ if [ -z "$HANA_HOSTS_IPS" ]; then
     exit 100
 fi
 
-if [ -z "$ASCS_PUBLIC_IP" ]; then
+if [ -z "$ASCS_PRIVATE_IP" ]; then
     echo "No ASCS host IP was received as input params"
     exit 100
 fi
 
-if [ -z "$ERS_PUBLIC_IP" ]; then
+if [ -z "$ERS_PRIVATE_IP" ]; then
     echo "No ERS host IP was received as input params"
     exit 100
 fi
 
-if [ -z "$PAS_PUBLIC_IP" ]; then
+if [ -z "$PAS_PRIVATE_IP" ]; then
     echo "No PAS host IP was received as input params"
     exit 100
 fi
 
-public_ips_values=$(echo $HANA_HOSTS_IPS | sed "s/\[/\ /g" | sed "s/\]/\ /g" | sed "s/\,/\ /g")
-eval "public_ips_array=($public_ips_values)"
+private_ips_values=$(echo $HANA_HOSTS_IPS | sed "s/\[/\ /g" | sed "s/\]/\ /g" | sed "s/\,/\ /g")
+eval "private_ips_array=($private_ips_values)"
 
-HANA_PRIMARY_PUBLIC_IP=${public_ips_array[0]}
-HANA_SECONDARY_PUBLIC_IP=${public_ips_array[1]}
+HANA_PRIMARY_PRIVATE_IP=${private_ips_array[0]}
+HANA_SECONDARY_PRIVATE_IP=${private_ips_array[1]}
 
 # ------------------------------------------------------------------
 # Create hosts_runtime.yml
@@ -48,11 +48,11 @@ HANA_SECONDARY_PUBLIC_IP=${public_ips_array[1]}
 cp "$ANSIBLE_DIR/hosts.yaml" "$ANSIBLE_DIR/hosts_runtime.yaml"
 hostsFile="$ANSIBLE_DIR/hosts_runtime.yaml"
 
-sed -i "s/HANA_PRIM_HOST_NAME_TO_APPLY/$HANA_PRIMARY_PUBLIC_IP/g" $hostsFile
-sed -i "s/HANA_SEC_HOST_NAME_TO_APPLY/$HANA_SECONDARY_PUBLIC_IP/g" $hostsFile
-sed -i "s/ASCS_HOST_NAME_TO_APPLY/$ASCS_PUBLIC_IP/g" $hostsFile
-sed -i "s/ERS_HOST_NAME_TO_APPLY/$ERS_PUBLIC_IP/g" $hostsFile
-sed -i "s/PAS_HOST_NAME_TO_APPLY/$PAS_PUBLIC_IP/g" $hostsFile
+sed -i "s/HANA_PRIM_HOST_NAME_TO_APPLY/$HANA_PRIMARY_PRIVATE_IP/g" $hostsFile
+sed -i "s/HANA_SEC_HOST_NAME_TO_APPLY/$HANA_SECONDARY_PRIVATE_IP/g" $hostsFile
+sed -i "s/ASCS_HOST_NAME_TO_APPLY/$ASCS_PRIVATE_IP/g" $hostsFile
+sed -i "s/ERS_HOST_NAME_TO_APPLY/$ERS_PRIVATE_IP/g" $hostsFile
+sed -i "s/PAS_HOST_NAME_TO_APPLY/$PAS_PRIVATE_IP/g" $hostsFile
 
 sed -i "s|PATH_TO_PEM_FILE|$SSH_KEYPAIR_FILE_CHKD|g" $hostsFile
 
